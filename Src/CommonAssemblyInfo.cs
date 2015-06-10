c80fb5e4aa4ba09c1482f0d82bac80d786bd7bd9
Tim Danner <tim.danner@solarwinds.com>
2015-06-10 14:00:06 -0500

    Remove source files from gh-pages branch.

--- Src/CommonAssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyCompany("SolarWinds, Inc.")]
-[assembly: AssemblyCopyright("Copyright © 2015 SolarWinds Worldwide, LLC.  All rights reserved.")]
--- Src/SwqlStudio/IConnectionTab.cs
+++
@@ -1,8 +0,0 @@
-namespace SwqlStudio
-{
-    public interface IConnectionTab
-    {
-        IApplicationService ApplicationService { get; set; }
-        ConnectionInfo ConnectionInfo { get; set; }
-    }
-}

cffe7fb5490c3cd77e52e27571b40fc6c0a79278
Tim Danner <tim.danner@solarwinds.com>
2015-06-25 14:49:24 -0500

    Remove unnecessary files from Install.

--- Src/Install/paraffin-setup.bat
+++
@@ -1,6 +0,0 @@
-paraffin -dir ..\Samples\VBClient -dirref SAMPLESDIR -alias ..\Samples\VBClient -groupname vbclient VBClient.wxs -e vspscc -e scc -x bin -x obj
-paraffin -dir ..\Samples\JavaClient -dirref SAMPLESDIR  -alias ..\Samples\JavaClient -groupname javaclient JavaClient.wxs -e class -x bin
-paraffin -dir ..\Samples\PerlClient -dirref SAMPLESDIR  -alias ..\Samples\PerlClient -groupname perlclient PerlClient.wxs
-paraffin -dir ..\Samples\PowerShell -dirref SAMPLESDIR -alias ..\Samples\PowerShell -groupname pssamples PowerShellSamples.wxs
-paraffin -dir ..\Samples\CSClient -dirref SAMPLESDIR -alias ..\Samples\CSClient -groupname csclient CSClient.wxs -x bin -x obj
-paraffin -dir ..\..\Doc\Schema -dirref DOCDIR  -alias ..\..\Doc\Schema -groupname schemadoc SchemaDoc.wxs

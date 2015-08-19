9651f99baecac4989c38ed71e9ddda92f77c65ba
unknown <Martin.Kunc@mkunc-dvbx.swdev.local>
2015-08-19 12:29:56 +0200

    Adding PowerShell EOC Swis Query Sample

---
+++ Samples/PowerShell/EOC.Query.ps1
@@ -0,0 +1,31 @@
+# This sample script demonstrates how to execute a query against EOC Classic
+# It returns list Uri of Orion Servers added to EOC
+# Please update the hostname and credential setup to match your configuration,
+# and reference to an existing node.
+
+# This .dll will be created after building OrionSDK.sln
+Add-Type -Path "..\..\Src\Contract\bin\Debug\SolarWinds.SDK.Swis.Contract.dll"
+
+if (! (Get-PSSnapin | where {$_.Name -eq "SwisSnapin"})) {
+    Add-PSSnapin "SwisSnapin"
+}
+
+# Connect to SWIS. EOC Swis uses Windows Account Authentication only
+$hostname = "localhost"
+$username = "Administrator"
+$password = "Password1" | ConvertTo-SecureString -asPlainText -Force
+
+$cred = new-Object SolarWinds.InformationService.Contract2.WindowsCredential("",$username,$password)
+
+$addr = New-Object Uri("net.tcp://" + $hostname + ":17777/SolarWinds/InformationService/EOC")
+$tcpBinding = New-Object System.ServiceModel.NetTcpBinding
+$tcpBinding.Security.Mode = [System.ServiceModel.SecurityMode]::Transport
+$tcpBinding.Security.Transport.ClientCredentialType = [System.ServiceModel.TcpClientCredentialType]::Windows
+
+$swis = New-Object SolarWinds.InformationService.Contract2.InfoServiceProxy( $addr, $tcpBinding, $cred )
+
+# Returns list of Orions in EOC. EOC Swis limits available Orions based on Manage Orions access rights to see Orions.
+# If user does not have rights to see Orion, query will not return its row
+$orionList = Get-SwisData $swis "SELECT Uri FROM EOC.Orion"
+
+Write-Host $orionList

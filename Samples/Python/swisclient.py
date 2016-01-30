cc0f27c5e3b33d2dc5c57442d8c717557bf736ea
Steven Klassen <sklassen@gmail.com>
2016-01-29 20:51:38 -0800

    Removed samples from the swisclient.py module.

--- Samples/Python/swisclient.py
+++ Samples/Python/swisclient.py
@@ -1,12 +1,6 @@
 import requests
 import json
-from getpass import getpass
-from datetime import datetime, timedelta
-
-
-"""
-Make sure to set a valid nodeID in line 55 before using!
-"""
+from datetime import datetime
 
 
 def _json_serial(obj):
@@ -53,64 +47,4 @@ class SwisClient:
                                 verify=False,
                                 auth=self.credentials,
                                 headers={'Content-Type': 'application/json'})
-
-requests.packages.urllib3.disable_warnings()
-
-
-def samplecode(npm_server, username, password):
-    swis = SwisClient(npm_server, username, password)
-
-    print("Invoke Test:")
-    aliases = swis.invoke(
-            "Metadata.Entity",
-            "GetAliases",
-            "SELECT B.Caption FROM Orion.Nodes B")
-    print(aliases)
-
-    print("Query Test:")
-    results = swis.query(
-            "SELECT Uri FROM Orion.Nodes WHERE NodeID=@id",
-            id=104)  # set valid NodeID!
-    uri = results['results'][0]['Uri']
-    print(uri)
-
-    print ("Custom Property Update Test:")
-    swis.update(uri + "/CustomProperties", city="Austin")
-    obj = swis.read(uri + "/CustomProperties")
-    print (obj)
-
-    pollerUri = swis.create(
-            "Orion.Pollers",
-            PollerType="just testing",
-            NetObject="N:" + str(obj["NodeID"]),
-            NetObjectType="N",
-            NetObjectID=obj["NodeID"])
-    print (pollerUri)
-
-    print ("Deleting temporary poller....")
-    swis.delete(pollerUri)
-
-
-def unmanage_a_node():
-    swis = SwisClient("localhost", "admin", "")
-    results = swis.query("SELECT TOP 1 NodeID FROM Orion.Nodes")
-    interfaceId = results["results"][0]["NodeID"]
-    netObjectId = "N:{}".format(interfaceId)
-    now = datetime.utcnow()
-    tomorrow = now + timedelta(days=1)
-    swis.invoke("Orion.Nodes", "Unmanage", netObjectId, now, tomorrow, False)
-
-
-def main():
-    # try: input = raw_input  # Fix Python 2.x
-    npm_server = input("IP address of NPM Server: ")
-    username = input("Username: ")
-    password = getpass("Password: ")
-
-    samplecode(npm_server, username, password)
-
-
-if __name__ == "__main__":
-    # main()
-    unmanage_a_node()
 

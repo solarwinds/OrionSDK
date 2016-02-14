f5ac9c9c8b2ed6a5ba4bc90096a413051dd2a1c1
Steven Klassen <sklassen@gmail.com>
2016-02-13 22:36:41 -0800

    Formatting changes in CRUD.SettingCustomProperty.ps1.

--- Samples/PowerShell/CRUD.SettingCustomProperty.ps1
+++ Samples/PowerShell/CRUD.SettingCustomProperty.ps1
@@ -11,19 +11,22 @@ $password = New-Object System.Security.SecureString
 $cred = New-Object -typename System.Management.Automation.PSCredential -argumentlist $username, $password
 $swis = Connect-Swis -host $hostname -cred $cred
 
-$nodeId=8 # NodeID of a node which custom properties you want to change
-$ifaceId=58 # InterfaceID of an interface on the node
+$nodeId = 8 # NodeID of a node which custom properties you want to change
+$ifaceId = 58 # InterfaceID of an interface on the node
 
 # prepare a custom property value
 $customProps = @{
-  Comments="Custom comment";
+    Comments="Custom comment";
 }
 
 # build the node URI
-$uri = "swis://localhost/Orion/Orion.Nodes/NodeID=$nodeId/CustomProperties";
+$uri = "swis://localhost/Orion/Orion.Nodes/NodeID=$($nodeId)/CustomProperties";
+
 # set the custom property
 Set-SwisObject $swis -Uri $uri -Properties $customProps
+
 # build the interface URI
-$uri = "swis://localhost/Orion/Orion.Nodes/NodeID=$nodeId/Interfaces/InterfaceID="+$ifaceId+"/CustomProperties";
+$uri = "swis://localhost/Orion/Orion.Nodes/NodeID=$($nodeId)/Interfaces/InterfaceID=$($ifaceId)/CustomProperties";
+
 # set the custom property
 Set-SwisObject $swis -Uri $uri -Properties $customProps

3bde637695324eb018e94a3b57c4688537733d2d
Tim Danner <tim.danner@solarwinds.com>
2017-02-21 14:22:22 -0600

    Fix warning

--- Src/SwqlStudio/MainForm.ApplicationService.cs
+++ Src/SwqlStudio/MainForm.ApplicationService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 using SwqlStudio.Metadata;
 using SwqlStudio.Subscriptions;
 
@@ -10,9 +6,6 @@ namespace SwqlStudio
 {
     partial class MainForm : IApplicationService
     {
-        public event EventHandler<IndicationEventArgs> IndicationReceived;
-
-
         public SubscriptionManager SubscriptionManager { get; } = new SubscriptionManager();
 
         public void AddTextToEditor(string text, ConnectionInfo info)

58446062acb5d78588dac8f334f7ce53d982786d
Tim Danner <tim.danner@solarwinds.com>
2017-04-03 23:27:12 -0500

    Add sample for alert suppression verbs

---
+++ Samples/PowerShell/AlertSuppression.ps1
@@ -0,0 +1,30 @@
+﻿if ( (Get-PSSnapin -Name SwisSnapin -ErrorAction SilentlyContinue) -eq $null )
+{
+    Add-PsSnapin SwisSnapin
+}
+
+$OrionServer = 'localhost'
+$Username = 'admin'
+$Password = ''
+
+$swis = Connect-Swis -Hostname $OrionServer -Username $Username -Password $Password
+
+# List alert suppressions
+Get-SwisData $swis "SELECT ID, EntityUri, SuppressFrom, SuppressUntil FROM Orion.AlertSuppression" | Format-Table
+
+# Get Uris for a couple of nodes
+$entityUris = Get-SwisData $swis "SELECT TOP 2 Uri FROM Orion.Nodes"
+$entityUris = $entityUris |% {[string]$_}
+
+# Test if some entities currently have alerts suppressed
+$result = Invoke-SwisVerb $swis Orion.AlertSuppression GetAlertSuppressionState @(,$entityUris)
+$result.EntityAlertSuppressionState
+
+# Suppress alerts for one or more entities
+$entityUris = Get-SwisData $swis "SELECT TOP 2 Uri FROM Orion.Nodes"
+$entityUris = $entityUris |% {[string]$_}
+
+Invoke-SwisVerb $swis Orion.AlertSuppression SuppressAlerts @($entityUris, [DateTime]::UtcNow) | Out-Null
+
+# End alert suppression for one or more entities
+Invoke-SwisVerb $swis Orion.AlertSuppression ResumeAlerts @(,$entityUris) | Out-Null

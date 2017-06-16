b3f3068f59fbe3dea129256a438b5ac42426ec8c
Tim Danner <tdanner@users.noreply.github.com>
2017-06-16 15:42:03 -0500

    Feature/pla 44 solarwinds.build (#78)
    * Remove Build from .gitignore
    
    * Add OrionSDK-Build as a submodule
    
    * Move installer to its own solution
    
    * Switch to SolarWinds.Build
    
    * Add NuGet.config
    
    * Move AssemblyVersion and AssemblyFileVersion to CommonAssemblyInfo.cs
    
    * Switch to SolarWinds.Build style of GlobalVariable.wxi file
    
    * Customize path of GlobalVariables.wxi
    
    * Update SolarWinds.Build plugin script
    
    * Update Build
    
    * Nuget restore for Install.sln handled by build script now
    
    * Update build scripts
    
    * Add a project.version file
    
    * Add nuget package restore to SwqlStudio.csproj

--- Src/CommonAssemblyInfo.cs
+++ Src/CommonAssemblyInfo.cs
@@ -2,3 +2,7 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("SolarWinds.")]
 [assembly: AssemblyCopyright("Copyright © 2015-2017 SolarWinds Worldwide, LLC.  All rights reserved.")]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

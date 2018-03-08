7e2925a9c5cd004e19389848e18884e0c80591ef
JirkaPok <j.pok@post.cz>
2018-03-08 18:36:07 +0100

    Extracted parameter updates from QueryParameters

---
+++ Src/SwqlStudio/CachedParameters.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SolarWinds.InformationService.Contract2;
+
+namespace SwqlStudio
+{
+    internal class CachedParameters
+    {
+        private readonly Dictionary<string, string> lastKnownValues = new Dictionary<string, string>();
+
+        internal void UpdateFromCachedValues(PropertyBag toUpdate, PropertyBag current)
+        {
+            UpdateFromLastKnown(toUpdate);
+            UpdateWithCurrentValues(toUpdate, current);
+            QuessRenamedParameter(toUpdate, current);
+        }
+
+        private void UpdateFromLastKnown(PropertyBag propertyBag)
+        {
+            foreach (string preservedKey in lastKnownValues.Keys.Where(propertyBag.ContainsKey))
+            {
+                propertyBag[preservedKey] = lastKnownValues[preservedKey];
+            }
+        }
+
+        private void UpdateWithCurrentValues(PropertyBag toUpdate, PropertyBag current)
+        {
+            foreach (var variable in current)
+            {
+                if (toUpdate.ContainsKey(variable.Key))
+                {
+                    toUpdate[variable.Key] = variable.Value;
+                }
+                else
+                {
+                    string lastKnownValue = variable.Value?.ToString();
+                        
+                    if (!String.IsNullOrEmpty(lastKnownValue))
+                        lastKnownValues[variable.Key] = lastKnownValue;
+                }
+            }
+        }
+
+        private void QuessRenamedParameter(PropertyBag propertyBag, PropertyBag currentVariables)
+        {
+            // we are able to identify only one renamed parameter using this simple concept
+            var added = propertyBag.Keys.Except(currentVariables.Keys).ToList();
+            var removed = currentVariables.Keys.Except(propertyBag.Keys).ToList();
+            if (added.Count == 1 && removed.Count == 1)
+            {
+                propertyBag[added.First()] = currentVariables[removed.First()];
+            }
+        }
+    }
+}
--- Src/SwqlStudio/QueryParameters.cs
+++ Src/SwqlStudio/QueryParameters.cs
@@ -8,7 +8,7 @@ namespace SwqlStudio
 {
     public partial class QueryParameters : DockContent
     {
-        private readonly Dictionary<string, string> lastKnownValues = new Dictionary<string, string>();
+        private CachedParameters preserved = new CachedParameters();
 
         public bool AllowSetParameters { get; set; }
 
@@ -35,25 +35,10 @@ namespace SwqlStudio
                 if (!this.AllowSetParameters)
                     return;
 
-                UpdateFromLastKnown(value);
-                var currentVariables = GetBoundQueryVariables();
-                UpdateWithCurrentValues(value, currentVariables);
-                var pairs = value.Select(pair => new QueryVariable(pair.Key, pair.Value?.ToString()));
-                QuessRenamedParameter(value);
-                var ordered = pairs.OrderBy(p => p.Key).ToList();
+                preserved.UpdateFromCachedValues(value, this.Parameters);
+                var ordered = value.Select(pair => new QueryVariable(pair.Key, pair.Value?.ToString()))
+                                   .OrderBy(p => p.Key).ToList();
                 parametersGrid.DataSource = new BindingList<QueryVariable>(ordered) { AllowNew = true };
-            }
-        }
-
-        private void QuessRenamedParameter(PropertyBag propertyBag)
-        {
-            // we are able to identify only one renamed parameter using this simple concept
-            var currentVariables = Parameters;
-            var added = propertyBag.Keys.Except(currentVariables.Keys).ToList();
-            var removed = currentVariables.Keys.Except(propertyBag.Keys).ToList();
-            if (added.Count == 1 && removed.Count == 1)
-            {
-                propertyBag[added.First()] = currentVariables[removed.First()];
             }
         }
 
@@ -61,29 +46,6 @@ namespace SwqlStudio
         {
             return ((BindingList<QueryVariable>)parametersGrid.DataSource)
                 .Where(v => v.Key != null);
-        }
-
-        private void UpdateWithCurrentValues(PropertyBag propertyBag, IEnumerable<QueryVariable> currentVariables)
-        {
-            foreach (QueryVariable variable in currentVariables)
-            {
-                if (propertyBag.ContainsKey(variable.Key))
-                {
-                    propertyBag[variable.Key] = variable.Value;
-                }
-                else if (!string.IsNullOrEmpty(variable.Value))
-                {
-                    lastKnownValues[variable.Key] = variable.Value;
-                }
-            }
-        }
-
-        private void UpdateFromLastKnown(PropertyBag propertyBag)
-        {
-            foreach (string preservedKey in lastKnownValues.Keys.Where(propertyBag.ContainsKey))
-            {
-                propertyBag[preservedKey] = lastKnownValues[preservedKey];
-            }
         }
     }
 }

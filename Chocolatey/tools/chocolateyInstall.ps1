46cf473ab5b346de6f82c7e09e4eaffaae294e51
Tim Danner <tim.danner@solarwinds.com>
2018-09-07 16:55:17 -0500

    Remove redundant "64" install details from chocolateyInstall.ps1

--- Chocolatey/tools/chocolateyInstall.ps1
+++ Chocolatey/tools/chocolateyInstall.ps1
@@ -5,14 +5,12 @@ $ErrorActionPreference = 'Stop';
 $packageName= 'orionsdk'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://github.com/solarwinds/OrionSDK/releases/download/v2.4.0.176/OrionSDK.msi'
-$url64      = $url
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'msi'
   url           = $url
-  url64bit      = $url64
 
   silentArgs    = "/qn /norestart"
   validExitCodes= @(0, 3010, 1641)
@@ -20,8 +18,6 @@ $packageArgs = @{
   softwareName  = 'orionsdk*'
   checksum      = '357d6e366164edbc3405142e00713b4a4a189017f52a4abb6e8268e75633c1c7'
   checksumType  = 'sha256'
-  checksum64    = '357d6e366164edbc3405142e00713b4a4a189017f52a4abb6e8268e75633c1c7'
-  checksumType64= 'sha256'
 }
 
 Install-ChocolateyPackage @packageArgs

75bf3f20363a22de7a87a145479caed9ed16e3f7
Dan Jagnow <dan.jagnow@solarwinds.com>
2019-04-04 12:45:56 -0500

    Updated chocolateyInstall.ps1 for the 2.5.0.214 release
    Updated the URL and SHA-256 checksum in chocolateyInstall.ps1 for the 2.5.0.214 release.

--- Chocolatey/tools/chocolateyInstall.ps1
+++ Chocolatey/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'orionsdk'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/solarwinds/OrionSDK/releases/download/v2.4.0.176/OrionSDK.msi'
+$url        = 'https://github.com/solarwinds/OrionSDK/releases/download/v2.5.0.214/OrionSDK.msi'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -16,7 +16,7 @@ $packageArgs = @{
   validExitCodes= @(0, 3010, 1641)
 
   softwareName  = 'orionsdk*'
-  checksum      = '357d6e366164edbc3405142e00713b4a4a189017f52a4abb6e8268e75633c1c7'
+  checksum      = 'A61FB923EC3D73E18061AD1F04B26C483E2526DDD5ABBEA9ABF6870CCD497AC6'
   checksumType  = 'sha256'
 }
 

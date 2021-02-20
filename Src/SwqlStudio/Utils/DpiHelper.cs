20593a463b046d4bfcf07326387d30294e4fb2f3
Peter Stranak <peter.stranak@solarwinds.com>
2021-02-20 23:13:35 +0100

    Fix DPI scaling for obsolete entities in ObjectTree

--- Src/SwqlStudio/Utils/DpiHelper.cs
+++ Src/SwqlStudio/Utils/DpiHelper.cs
@@ -3,33 +3,45 @@ using System.Windows.Forms;
 
 namespace SwqlStudio.Utils
 {
+    /// <summary>
+    /// A helper class for supporting font and DPI scaling
+    /// </summary>
+    /// <remarks>
+    /// <para>This dimensions of the default font are used to calculate a proper DPI scaling ratio of elements. .Net Framework tries to use "MS Sans Serif"
+    /// from Windows 3.1, which isn't available anymore. So the system automatically replaces it with "Microsoft Sans Serif", or sometimes with "Tahoma".
+    /// But those are too old and don't match with Windows UI fonts. And if there are elements using also other fonts, like "Segoe UI", it messes up the calculation.
+    /// That can happen especially when a different font is used for rendering than for the DPI scaling calculation.</para>
+    /// <para>This should not be needed anymore in .Net Core 3.1 and .Net 5: https://github.com/dotnet/winforms/pull/656 </para>
+    /// </remarks>
     internal static class DpiHelper
     {
         /// <summary>
-        /// Sets the currently used system UI font as the default font for the <paramref name="control"/>
+        /// The default font used in SWQL Studio dialogs
+        /// </summary>
+        /// <remarks>Returns the default font used by the OS.</remarks>
+        /// <value><see cref="SystemFonts.MessageBoxFont"/></value>
+        public static readonly Font DefaultFont = SystemFonts.MessageBoxFont;
+
+        /// <summary>
+        /// Sets the <see cref="DefaultFont"/> as the default font for the <paramref name="control"/>
         /// </summary>
         /// <param name="control">Usually a <see cref="Form"/></param>
-        /// <remarks>This is needed because dimensions of the default font are used to calculate a proper DPI scaling ratio of the <paramref name="control"/>'s child elements.
-        /// <para>.Net Framework tries to use "MS Sans Serif" from Windows 3.1, which isn't available anymore. So the system automatically replaces it with "Microsoft Sans Serif",
-        /// or sometimes with "Tahoma". But those are also too old. And if there are elements using also other fonts, like "Segoe UI", it messes up the calculation.
-        /// So, instead of hard-coding a specific font, let's use whatever the OS prefers to use for its own dialogs.</para>
-        /// <para>This has been reportedly fixed in .Net Core and .Net 5.</para></remarks>
+        /// <remarks>This is needed to correctly calculate a proper DPI scaling ratio of the <paramref name="control"/> and its child elements.
+        /// It forces the <paramref name="control"/> to sync the fonts used for its rendering and DPI scaling calculation.
+        /// <para>This should be called in the <paramref name="control"/>'s constructor.</para></remarks>
         public static void FixFont(Control control)
         {
-            control.Font = SystemFonts.MessageBoxFont;
+            control.Font = DefaultFont;
         }
 
         /// <summary>
         /// Adjusts the default row height in a <see cref="DataGridView"/>
         /// </summary>
         /// <remarks>The default row height is defined as the height of the default font + 9 pixels.
-        /// However, because the default font in .Net Framework is "MS Sans Serif 8.25F", it's too small for fonts like "Segoe UI, 9F",
-        /// especially when DPI scaling is involved.
-        /// <para>This method, when called after <see cref="FixFont"/>, forces the recalculation of the height.</para>
-        /// <para>This has been reportedly fixed in .Net Core and .Net 5.</para></remarks>
+        /// This method, when called after <see cref="FixFont"/>, forces the recalculation of the height.</remarks>
         public static void FixRowHeight(DataGridView dataGridView)
         {
-            // Instead of "dataGridView.DefaultCellStyle.Font.Height" we could just hard-code "SystemFonts.MessageBoxFont". That way, this method would not depend
+            // Instead of "dataGridView.DefaultCellStyle.Font.Height" we could just hard-code "DefaultFont". That way, this method would not depend
             // on a previous call to "FixFont". But this is a bit more universal solution.
             dataGridView.RowTemplate.Height = dataGridView.DefaultCellStyle.Font.Height + 9;
         }

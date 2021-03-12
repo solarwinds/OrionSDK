daad30cde9a7879287a20178dae1cee6b2047942
Dan Jagnow <dan.jagnow@solarwinds.com>
2021-03-12 13:50:43 -0600

    Removed unused EmptyCredentials class
    Removed unused EmptyCredentials class and static None property from the ServiceCredentials class.

--- Src/Contract/ServiceCredentials.cs
+++ Src/Contract/ServiceCredentials.cs
@@ -7,29 +7,6 @@ namespace SolarWinds.InformationService.Contract2
     [DataContract(Namespace = "http://schema.solarwinds.com/2007/08/IS")]
     public abstract class ServiceCredentials : IDisposable
     {
-        private readonly static EmptyCredentials _emptyCredentials = new EmptyCredentials();
-
-        #region EmptyCredentials class
-
-        private class EmptyCredentials : ServiceCredentials
-        {
-            public override CredentialType CredentialType
-            {
-                get { return CredentialType.None; }
-            }
-
-            public override void ApplyTo(ChannelFactory channelFactory)
-            {
-            }
-        }
-
-        #endregion
-
-        public static ServiceCredentials None
-        {
-            get { return _emptyCredentials; }
-        }
-
         public abstract CredentialType CredentialType { get; }
 
         public abstract void ApplyTo(ChannelFactory channelFactory);

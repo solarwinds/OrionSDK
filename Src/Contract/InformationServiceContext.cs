9d49f44f7b0197119e0e17a9ddd7778466faaf03
Dan Jagnow <dan.jagnow@solarwinds.com>
2021-03-12 13:35:03 -0600

    Removed unused members from InformationServiceContext
    Removed several unused constructors and a CreateQuery overload from InformationServiceContext.

--- Src/Contract/InformationServiceContext.cs
+++ Src/Contract/InformationServiceContext.cs
@@ -12,58 +12,6 @@ namespace SolarWinds.InformationService.Contract2
             this.service = service;
         }
 
-        public InformationServiceContext(string endpointName)
-        {
-            if (endpointName == null)
-                throw new ArgumentNullException(nameof(endpointName));
-            if (endpointName.Length == 0)
-                throw new ArgumentException("endpoint name is empty", nameof(endpointName));
-
-            Proxy = new InfoServiceProxy(endpointName);
-            Proxy.Open();
-        }
-
-        public InformationServiceContext(string endpointName, string remoteAddress)
-        {
-            if (endpointName == null)
-                throw new ArgumentNullException(nameof(endpointName));
-            if (endpointName.Length == 0)
-                throw new ArgumentException("endpoint name is empty", nameof(endpointName));
-            if (remoteAddress == null)
-                throw new ArgumentNullException(nameof(remoteAddress));
-
-            Proxy = new InfoServiceProxy(endpointName, remoteAddress);
-            Proxy.Open();
-        }
-
-        public InformationServiceContext(string endpointName, ServiceCredentials credentials)
-        {
-            if (endpointName == null)
-                throw new ArgumentNullException(nameof(endpointName));
-            if (endpointName.Length == 0)
-                throw new ArgumentException("endpoint name is empty", nameof(endpointName));
-            if (credentials == null)
-                throw new ArgumentNullException(nameof(credentials));
-
-            Proxy = new InfoServiceProxy(endpointName, credentials);
-            Proxy.Open();
-        }
-
-        public InformationServiceContext(string endpointName, string remoteAddress, ServiceCredentials credentials)
-        {
-            if (endpointName == null)
-                throw new ArgumentNullException(nameof(endpointName));
-            if (endpointName.Length == 0)
-                throw new ArgumentException("endpoint name is empty", nameof(endpointName));
-            if (remoteAddress == null)
-                throw new ArgumentNullException(nameof(remoteAddress));
-            if (credentials == null)
-                throw new ArgumentNullException(nameof(credentials));
-
-            Proxy = new InfoServiceProxy(endpointName, remoteAddress, credentials);
-            Proxy.Open();
-        }
-
         /// <summary>
         /// Creates query against SWIS.
         /// If <typeparamref name="T"/> has method Add(string,object) you can use <see cref="IEntityPropertySetter"/> interface to improve performance.
@@ -79,24 +27,6 @@ namespace SolarWinds.InformationService.Contract2
                 throw new InvalidOperationException("context disposed");
 
             return new InformationServiceQuery<T>(this, queryString);
-        }
-
-        /// <summary>
-        /// Creates query against SWIS.
-        /// If <typeparamref name="T"/> has method Add(string,object) you can use <see cref="IEntityPropertySetter"/> interface to improve performance.
-        /// </summary>
-        /// <typeparam name="T">type of returned entity</typeparam>
-        /// <param name="queryString">query in SWQL</param>
-        /// <param name="parameters">query parameters</param>
-        /// <returns>result of query execution</returns>
-        public InformationServiceQuery<T> CreateQuery<T>(string queryString, PropertyBag parameters) where T : new()
-        {
-            if (queryString == null)
-                throw new ArgumentNullException(nameof(queryString));
-            if (disposed)
-                throw new InvalidOperationException("context disposed");
-
-            return new InformationServiceQuery<T>(this, queryString, parameters);
         }
 
         public InfoServiceProxy Proxy { get; private set; } = null;

5a604c698dec28726bcca07590731f3432e4e3e6
Dan Jagnow <dan.jagnow@solarwinds.com>
2021-04-09 17:43:47 -0500

    Customized docusaurus.config.js
    Customized docusaurus.config.js with settings appropriate for the Orion SDK.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,19 +1,19 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  title: 'Orion SDK',
+  tagline: 'Using the SolarWinds Orion platform API',
+  url: 'http://solarwinds.github.io',
+  baseUrl: '/OrionSDK/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'solarwinds', // Usually your GitHub org/user name.
+  projectName: 'OrionSDK', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'Orion SDK',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'SolarWinds Logo',
         src: 'img/logo.svg',
       },
       items: [
@@ -25,7 +25,7 @@ module.exports = {
         },
         {to: 'blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/solarwinds/OrionSDK',
           label: 'GitHub',
           position: 'right',
         },
@@ -47,16 +47,16 @@ module.exports = {
           title: 'Community',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              label: 'THWACK',
+              href: 'https://thwack.solarwinds.com/product-forums/the-orion-platform/f/orion-sdk',
             },
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/solarwinds-orion',
             },
             {
               label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
+              href: 'https://twitter.com/solarwinds',
             },
           ],
         },
@@ -69,12 +69,12 @@ module.exports = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/solarwinds/OrionSDK',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} SolarWinds Worldwide, LLC. All rights reserved.`,
     },
   },
   presets: [
@@ -85,13 +85,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/solarwinds/OrionSDK/edit/documentation/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/solarwinds/OrionSDK/edit/documentation/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),

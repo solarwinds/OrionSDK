26990f9b935328e8dc5fa92fffd0f32906fa2903
Michal Ludik <michal.ludik@gmail.com>
2024-09-04 15:40:50 +0200

    Added documentation for 2024.4

--- swagger-ui/swagger-initializer.js
+++ swagger-ui/swagger-initializer.js
@@ -4,6 +4,7 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     urls: [
+      {url:"https://solarwinds.github.io/OrionSDK/2024.4/swagger.json", name:"2024.4"},
       {url:"https://solarwinds.github.io/OrionSDK/2024.2/swagger.json", name:"2024.2"},
       {url:"https://solarwinds.github.io/OrionSDK/2024.1/swagger.json", name:"2024.1"},
       {url:"https://solarwinds.github.io/OrionSDK/2023.4/swagger.json", name:"2023.4"},
